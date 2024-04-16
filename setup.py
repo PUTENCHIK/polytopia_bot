20e40bd348d8ce493bc9a720fac8715fccc8b4e9
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-04-16 03:21:39 +0000

    Done setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-   name='polytopia-bot',
+   name='polytopia_bot',
    version='0.5',
    description='Legendary the hardest bot for game The Battle of Polytopia',
    license='MIT',
    author='Maxim Olifirenko',
    author_email='opaolifirenkomaxim@gmail.com',
    url='https://github.com/PUTENCHIK/polytopia-bot',
-   packages=['mtracker'],
+   packages=['polytopia_bot'],
    install_requires=[], # it is empty since we use standard python library
    extras_require={
         'test': [
